ce0946f0f06b7231e674f77c79e7184cafa19a9e
acanod <alejandro.cano@opendeusto.es>
2021-11-26 22:45:12 +0100

    feat: New routes and new methods added for users
    Get users
    Modify a user by id
    Delete a user by id

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,6 +32,17 @@ export const createUser = async (req, res) => {
     }
 };
 
-export const getUsers = async (req, res) => { };
+export const getUsers = async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+};
 
-export const getUser = async (req, res) => { };
+export const getUserById = async (req, res) => {
+    const user = await User.findById(req.params.userId);
+    res.status(200).json(user);
+};
+
+export const deleteUserById = async (req, res) => {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(204).json();
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-const router = Router();
-
 import * as usersCtrl from "../controllers/userController";
 import { authJwt, verifySignup } from "../middlewares";
 
+const router = Router();
+
 router.post("/", [authJwt.verifyToken, authJwt.isAdmin, verifySignup.checkDuplicateUsernameOrEmail], usersCtrl.createUser);
+
+router.get("/", [authJwt.verifyToken, authJwt.isModerator], usersCtrl.getUsers);
+
+router.get("/:userId", [authJwt.verifyToken, authJwt.isModerator], usersCtrl.getUserById);
+
+router.delete("/:userId", [authJwt.verifyToken, authJwt.isAdmin], usersCtrl.deleteUserById);
 
 export default router;
